aaa3eacd7ace00d828ade46672a0a30771b2db05
Карбышев Олег Сергеевич <KarbyshevOSe@ugsk.ru>
2022-04-27 18:36:59 +0500

    Готово

---
+++ app/components/Converter.vue
@@ -0,0 +1,97 @@
+<template>
+  <Page class="page">
+    <ActionBar title="Конвертер" class="action-bar">
+      <NavigationButton
+        text="Назад"
+        android.systemIcon="ic_menu_back"
+        @tap="goBack"
+      />
+    </ActionBar>
+    <ScrollView>
+      <FlexboxLayout flexDirection="column" justifyContent="center">
+        <FlexboxLayout
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          class="converterType"
+          row="0"
+          col="0"
+          @tap="goTo('measure')"
+        >
+          <Label> Длина и расстояние </Label>
+        </FlexboxLayout>
+        <FlexboxLayout
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          class="converterType"
+          row="0"
+          col="1"
+          @tap="goTo('temp')"
+        >
+          <Label> Температура </Label>
+        </FlexboxLayout>
+        <FlexboxLayout
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          class="converterType"
+          row="1"
+          col="0"
+          @tap="goTo('binary')"
+        >
+          <Label textWrap> Единицы хранения цифровой информации </Label>
+        </FlexboxLayout>
+      </FlexboxLayout>
+    </ScrollView>
+  </Page>
+</template>
+
+<script>
+import Home from "./Home.vue";
+
+import Measure from "./converterTypes/measure.vue";
+import Temp from "./converterTypes/temp.vue";
+import Binary from "./converterTypes/binary.vue";
+
+export default {
+  name: "Converter",
+  methods: {
+    goTo(place) {
+      switch (place) {
+        case "measure":
+          this.$navigateTo(Measure);
+          break;
+        case "temp":
+          this.$navigateTo(Temp);
+          break;
+        case "binary":
+          this.$navigateTo(Binary);
+          break;
+      }
+    },
+    goBack() {
+      this.$navigateTo(Home);
+    },
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+.converterType {
+  margin: 50px;
+  height: 200px;
+  background-color: #3c3c3c;
+  text-align: center;
+  font-size: 12px;
+}
+.iconConverter {
+  width: 190px;
+  height: 190px;
+  margin-bottom: 30px;
+}
+</style>
---
+++ app/components/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <Page class="page">
+    <ActionBar title="Kilanov" class="action-bar" />
+    <ScrollView>
+      <FlexboxLayout flexDirection="column" justifyContent="center">
+        <Button
+          style="font-size: 14px"
+          text="Калькулятор"
+          @tap="goToDetailPage('calc')"
+        />
+        <Button
+          style="font-size: 14px"
+          text="Конвертер"
+          @tap="goToDetailPage('converter')"
+        />
+        <Button
+          style="font-size: 14px"
+          text="Календарь"
+          @tap="goToDetailPage('calendar')"
+        />
+      </FlexboxLayout>
+    </ScrollView>
+  </Page>
+</template>
+
+<script>
+import Calc from "./Calc.vue";
+import Converter from "./Converter.vue";
+import Calendar from "./Calendar.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    goToDetailPage(page) {
+      switch (page) {
+        case "calc":
+          this.$navigateTo(Calc);
+          break;
+        case "converter":
+          this.$navigateTo(Converter);
+          break;
+        case "calendar":
+          this.$navigateTo(Calendar);
+          break;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.kp {
+  width: 400px;
+  height: 300px;
+}
+</style>
